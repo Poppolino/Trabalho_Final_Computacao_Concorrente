0d6aadf22385a5bf9521ab3f9a2bd78d2fa5f186
Pedro Poppolino <57730364+Poppolino@users.noreply.github.com>
2022-02-02 03:40:51 -0300

    Gerador de Casos de Teste Aleatórios

--- Casos de Teste/Desempenho/gera_teste.c
+++ Casos de Teste/Desempenho/gera_teste.c
@@ -25,60 +25,60 @@ int num_aleatorio(int ini, int fim){
 
 
 int main(int argc, char *argv[]){
-	FILE *arq;
-	char *nome_arq;
-	int vertices, arestas, a, b, peso;
+    FILE *arq;
+    char *nome_arq;
+    int vertices, arestas, a, b, peso;
 
-	
-	/* Encerra o programa se tiver sido executado com pouca informação */
-	if(argc < 4){
-		printf("Escreva o padrão: %s <Número de Vértices> <Número de arestas> <Nome do Novo Arquivo>\n\n", argv[0]);
-		return 1;
-	}
-	
-	
-	/* Converte o dados de entrada para valores inteiros e pega o nome do arquivo */
-	vertices = atoi(argv[1]);
-	arestas = atoi(argv[2]);
-	nome_arq = argv[3];
+    
+    /* Encerra o programa se tiver sido executado com pouca informação */
+    if(argc < 4){
+        printf("Escreva o padrão: %s <Número de Vértices> <Número de arestas> <Nome do Novo Arquivo>\n\n", argv[0]);
+        return 1;
+    }
+    
+    
+    /* Converte o dados de entrada para valores inteiros e pega o nome do arquivo */
+    vertices = atoi(argv[1]);
+    arestas = atoi(argv[2]);
+    nome_arq = argv[3];
 
-	
-	/* Se não houver vértices ou se tiver dimensão negativa, termina o programa */
+    
+    /* Se não houver vértices ou se tiver dimensão negativa, termina o programa */
     if(vertices <= 0 || arestas < 0){
         printf("O grafo deve ter pelo menos um vértice e o número de arestas deve ser não negativo.\n\n"); 
         return 1;
     }
-	
-	
-	/* Abre um arquivo para escrita */
-	arq = fopen(nome_arq, "w");
-	
-	/* Encerra o programa caso não consiga criar o arquivo */
-	if(arq == (FILE *)NULL){
-		printf("Problema na criação do arquivo.\n\n");
-		return 2;
-	}
+    
+    
+    /* Abre um arquivo para escrita */
+    arq = fopen(nome_arq, "w");
+    
+    /* Encerra o programa caso não consiga criar o arquivo */
+    if(arq == (FILE *)NULL){
+        printf("Problema na criação do arquivo.\n\n");
+        return 2;
+    }
 
 
-	/* Escreve o número de vértices e de arestas no arquivo */
-	fprintf(arq, "%d %d\n", vertices, arestas);
+    /* Escreve o número de vértices e de arestas no arquivo */
+    fprintf(arq, "%d %d\n", vertices, arestas);
 
 
-	/* 
-		Gera aleatoriamente as arestas, evitando pesos negativos 
-		para não gerar ciclos negativos.
-	*/
-	for(int i=0; i<arestas; i++){
-		a = num_aleatorio(1, vertices);
-		b = num_aleatorio(1, vertices);
-		peso = num_aleatorio(1, arestas);
-		
-		fprintf(arq, "%d %d %d\n", a, b, peso);
-	}
+    /* 
+        Gera aleatoriamente as arestas, evitando pesos negativos 
+        para não gerar ciclos negativos.
+    */
+    for(int i=0; i<arestas; i++){
+        a = num_aleatorio(1, vertices);
+        b = num_aleatorio(1, vertices);
+        peso = num_aleatorio(1, arestas);
+        
+        fprintf(arq, "%d %d %d\n", a, b, peso);
+    }
 
 
-	/* Fecha o arquivo de escrita */
-	fclose(arq);
+    /* Fecha o arquivo de escrita */
+    fclose(arq);
 
-	return 0;
+    return 0;
 }
